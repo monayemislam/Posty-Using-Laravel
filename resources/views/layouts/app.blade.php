83faff5d64c2cc156e51fc8677c3239f05bb038d
Monayem Islam <monayem.dev@gmail.com>
2022-03-23 11:46:58 +0600

    Tailwind CSS removed and bootstrap 5 added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstrap 5-->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
     <title>Document</title>
 </head>
 <body>
